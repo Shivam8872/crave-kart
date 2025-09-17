727498a675b6f7b71e0d7802c85ff820930715d8
Shivam8872 <globalmindset10@gmail.com>
2025-09-17 15:12:39 +0530

    Update AddressSelector.tsx

--- client/src/components/AddressSelector.tsx
+++ client/src/components/AddressSelector.tsx
@@ -407,7 +407,7 @@ const AddressSelector: React.FC<AddressSelectorProps> = ({
       )}
 
       <Dialog open={dialogOpen} onOpenChange={setDialogOpen}>
-  <DialogContent className="sm:max-w-[525px] w-[95%] max-h-[90vh] overflow-y-auto">
+  <DialogContent className="sm:max-w-[525px] w-[95%] max-h-[90vh] overflow-y-auto pb-28 
           <DialogHeader>
             <DialogTitle>Add New Address</DialogTitle>
             <DialogDescription>
