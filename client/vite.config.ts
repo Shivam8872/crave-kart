fc27406074bb6331172a21f41d87d44e68d3e050
Shivam8872 <avadhkishorsah62@gmail.com>
2025-09-18 21:10:07 +0530

    d

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // If you see a warning about browserslist/caniuse-lite being outdated, run:
 // npm run update-db (server) or npx update-browserslist-db@latest
@@ -13,9 +12,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
